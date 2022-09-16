85c4fcfbb15a8268b5ec222f6248180f09e0b14a
Than Hutchins <mynockspit@gmail.com>
2022-09-16 00:53:50 -0700

    make command more easily editable

--- server.js
+++ server.js
@@ -12,14 +12,8 @@ let nb = path.resolve(__dirname, 'nb.js')
 
 const db = flatfile.sync(path.join(__dirname, 'server-config.db'))
 
-// this function is only used by the web code
-// do not add any node-specific stuff in it
-// do not run it outside of the rendered output
-async function doCommand(event) {
-  event.preventDefault()
-  let text = await (await fetch(window.location.pathname, { method: 'POST' })).text()
-  let outputElement = document.querySelector('#output')
-  outputElement.innerHTML += text
+function changeCommand(event) {
+
 }
 
 function makeCommandsLinks(ctx, input) {
@@ -68,16 +62,52 @@ function sanitizeHtml(input) {
 }
 
 function runAndRenderPage(ctx, commandString) {
+  // this function is only used by the web code
+  // do not add any node-specific stuff in it
+  // do not run it outside of the rendered output
+  async function doCommand(event) {
+    event.preventDefault()
+    let text = await (await fetch(window.location.pathname, { method: 'POST' })).text()
+    let outputElement = document.querySelector('#output')
+    outputElement.innerHTML += text
+  }
+
   return `<script>${doCommand.toString()}</script><pre>\n` +
-    makeCommandsLinks(ctx, runAndRender(ctx, `${commandString} --help`)) +
+    makeCommandsLinks(ctx, runAndRender(ctx, commandString, '--help')) +
     `\n\n` +
     `<a href='#' onclick="doCommand(event)">run <b>nb.js ${commandString}</b></a>` +
     `\n</pre>` +
     `<pre id="output"></pre>`
 }
 
-function runAndRender(ctx, commandString) {
-  let command = `${nb} ${commandString}`
+function form(prefix, defaultValue, suffix) {
+  function onInput(event) {
+    document.querySelector('#spacer').innerHTML = ` ${event.target.value} `
+  }
+
+  function focusInput() {
+    document.querySelector('#command-input').focus()
+  }
+
+  function goToCommand(event) {
+    event.preventDefault()
+    window.location.pathname = `/nb/${encodeURIComponent(event.target[0].value)}`
+  }
+
+  let formStyle = `style="white-space: normal; display: inline-block;" `
+  let spanStyle = `style="white-space: pre;" `
+  let wrapperStyle = `style="position: relative; display: inline-block;" `
+  let inputStyle = `style="position: absolute; width: 100%; left: 0; border: 0; padding: 0; margin: 0; font-family: inherit; font-size: inherit; text-align: center;" `
+  return `<script>${onInput.toString()}; ${focusInput.toString()}; ${goToCommand.toString()};</script><form ${formStyle} onclick="focusInput()" onsubmit="goToCommand(event)">
+    <span ${spanStyle}>${prefix.trim()}</span><span ${wrapperStyle}>
+      <span id="spacer" ${spanStyle}> ${defaultValue} </span>
+      <input id="command-input" ${inputStyle} oninput="onInput(event)" value="${defaultValue.replace(/"/g,'\\"')}"></input>
+    </span><span ${spanStyle}>${suffix}</span>
+  </form>`
+}
+
+function runAndRender(ctx, commandString, nonEditableFlags) {
+  let command = `${nb} ${commandString} ${nonEditableFlags}`
   let output
   try {
     output = execSync(command, { encoding: 'utf8' })
@@ -88,7 +118,7 @@ function runAndRender(ctx, commandString) {
   console.log(`> ${command}`)
   console.log(output)
 
-  return `> nb.js ${sanitizeHtml(commandString)}\n\n`
+  return `> ${form(`nb.js `, sanitizeHtml(commandString), nonEditableFlags)}\n\n`
     + sanitizeHtml(output)
 }
 
